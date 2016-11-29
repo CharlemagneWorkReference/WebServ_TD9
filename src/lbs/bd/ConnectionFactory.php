bc37dd0dfc42f45326ec664e52c4bb3f03a24c42
Cyprien Aubry <cyprienaubry@linkpulsion.com>
2016-11-29 15:05:50 +0100

    Connection Factory finished

--- src/lbs/bd/ConnectionFactory.php
+++ src/lbs/bd/ConnectionFactory.php
@@ -3,14 +3,34 @@
 namespace lbs\bd;
 
 use Exception;
+use ParseError;
+use PDO;
 
 class ConnectionFactory
 {
+
+    private static $PDO;
+    private static $config;
+
     public static function setConfig($file)
     {
         if (!isset($file)){
-            throw new Exception("Aucun fichier de configuration chargé");
+            throw new Exception('Aucun fichier de configuration chargé');
         }
+        try {
+            self::$config = parse_ini_file($file);
+        } catch (ParseError $e){
+            die($e -> getMessage());
+        }
+    }
 
+    public static function makeConnection()
+    {
+        if (is_null(self::$PDO) && !is_null(self::$config)){
+            self::$PDO = new PDO(self::$config);
+        } else {
+            throw new Exception('Configuration is null');
+        }
+        return self::$PDO;
     }
 }
